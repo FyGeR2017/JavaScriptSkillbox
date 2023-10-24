07d8958075b10e5b64d9b847f34cd094edb61a6b
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2023-10-24 18:49:41 +0500

    Add files via upload

---
+++ mod5/task1.js
@@ -0,0 +1,9 @@
+let currentYear = 2023
+
+function getAge(birthYear){   
+    return currentYear - birthYear;
+}
+
+console.log(getAge(1998))
+console.log(getAge(1991))
+console.log(getAge(2007))
---
+++ mod5/task2.js
@@ -0,0 +1,16 @@
+let whiteList = ['my-email@gmail.ru', 'jsfunc@mail.ru', 'annavkmail@vk.ru', 'fullname@skill.ru', 'goodday@day.ru']
+let blackList = ['jsfunc@mail.ru','goodday@day.ru']
+
+function filter(whiteList,blackList){
+    let result = []
+
+    for (let i = 0; i < whiteList.length; i++){
+        if (!blackList.includes(whiteList[i])){
+            result.push(whiteList[i])
+        }
+    }
+    
+    return result
+}
+
+console.log(filter(whiteList,blackList))
