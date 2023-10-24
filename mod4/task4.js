6c5bf8192c39c639129d671fda35ae8e49eadb49
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2023-10-24 15:30:42 +0500

    Update task4.js

--- mod4/task4.js
+++ mod4/task4.js
@@ -1,22 +1,6 @@
-function concatArrays(arr1,arr2){
+function concatArrays(arr1,arr2){   
     
-    let result = [];
-
-    let i = 0;
-    let j = 0;
-
-    while (i < arr1.length || j < arr2.length){
-        if(i < arr1.length){
-            result.push(arr1[i]);
-            i++;
-        }
-        if(j < arr2.length){
-            result.push(arr2[j]);
-            j++;
-        }
-    } 
-    
-    return result;
+    return arr1.concat(arr2);
 }
 
 console.log(concatArrays([2, 2, 17, 21, 45, 12, 54, 31, 53],[12, 44, 23, 5]))
