fd57046082eb2b8cdf92f4b596394d1f1b08456a
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2023-10-24 23:02:56 +0500

    Update task1.js

--- mod5/task1.js
+++ mod5/task1.js
@@ -1,10 +1,10 @@
-let currentYear = 2023
+let currentYear = 2023;
 
 function getAge(birthYear){  
     
     return currentYear - birthYear;
 }
 
-console.log(getAge(1998))
-console.log(getAge(1991))
-console.log(getAge(2007))
+console.log(getAge(1998));
+console.log(getAge(1991));
+console.log(getAge(2007));
