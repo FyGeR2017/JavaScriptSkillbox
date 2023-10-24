35fd6a67bb57aa838c1e26403d05aed3427a1def
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2023-10-24 23:02:25 +0500

    Update task3.js

--- mod5/task3.js
+++ mod5/task3.js
@@ -3,9 +3,9 @@ function arrSort(array){
     for (let i = 0; i < array.length; i++){
         for ( let j = 0; j < array.length - 1; j++){
             if (array[i+1] < array[j]){
-                let temp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = temp
+                let temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
             }
         }        
     }
