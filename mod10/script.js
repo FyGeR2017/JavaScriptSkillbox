2b4e885899bd44838ca0fb2f1d38c86b1571abc0
FyGeR2017 <91911137+FyGeR2017@users.noreply.github.com>
2023-11-25 22:10:25 +0500

    Update script.js

--- mod10/script.js
+++ mod10/script.js
@@ -1,40 +1,34 @@
-const students = [
-    {
-        firstName: "Александр",
-        lastName: "Проскуряков",
-        middleName: "Сергеевич",
-        dob: new Date("2005-03-28"),
-        startYear: 2022,
-        faculty: "ИРИТ-РТФ"
-    },
-    {
-        firstName: "Богдан",
-        lastName: "Бикаев",
-        middleName: "Александрович",
-        dob: new Date("2004-04-08"),
-        startYear: 2022,
-        faculty: "ИРИТ-РТФ"
-    },
-    {
-        firstName: "Данил",
-        lastName: "Китушкин",
-        middleName: "Яковлевич",
-        dob: new Date("2003-01-25"),
-        startYear: 2021,
-        faculty: "ИРИТ-РТФ"
-    }
-];
+const students = [{
+    firstName: "Александр",
+    lastName: "Проскуряков",
+    middleName: "Сергеевич",
+    dob: new Date("2005-03-28"),
+    startYear: 2022,
+    faculty: "ИРИТ-РТФ"
+}, {
+    firstName: "Богдан",
+    lastName: "Бикаев",
+    middleName: "Александрович",
+    dob: new Date("2004-04-08"),
+    startYear: 2022,
+    faculty: "ИРИТ-РТФ"
+}, {
+    firstName: "Данил",
+    lastName: "Китушкин",
+    middleName: "Яковлевич",
+    dob: new Date("2003-01-25"),
+    startYear: 2021,
+    faculty: "ИРИТ-РТФ"
+}];
 
 function addStudent(event) {
     event.preventDefault();
-
     const firstName = document.getElementById('first-name').value;
     const lastName = document.getElementById('last-name').value;
     const middleName = document.getElementById('middle-name').value;
     const dob = new Date(document.getElementById('dob').value);
     const startYear = parseInt(document.getElementById('start-year').value);
     const faculty = document.getElementById('faculty').value;
-
     const student = {
         firstName,
         lastName,
@@ -43,7 +37,6 @@ function addStudent(event) {
         startYear,
         faculty
     };
-
     students.push(student);
     renderStudentsTable();
     document.getElementById('add-student-form').reset();
@@ -52,41 +45,32 @@ function addStudent(event) {
 function renderStudentsTable(filteredStudents = students) {
     const studentsList = document.getElementById('students-list');
     studentsList.innerHTML = '';
-
     filteredStudents.forEach((student) => {
         const row = document.createElement('tr');
-
         const firstNameCell = document.createElement('td');
         firstNameCell.textContent = student.firstName;
         row.appendChild(firstNameCell);
-
         const lastNameCell = document.createElement('td');
         lastNameCell.textContent = student.lastName;
         row.appendChild(lastNameCell);
-
         const middleNameCell = document.createElement('td');
         middleNameCell.textContent = student.middleName;
         row.appendChild(middleNameCell);
-
         const dobCell = document.createElement('td');
         const dob = student.dob.toLocaleDateString('ru-RU');
         const age = calculateAge(student.dob);
         dobCell.textContent = `${dob} (${age} лет)`;
         row.appendChild(dobCell);
-
         const startYearCell = document.createElement('td');
         startYearCell.textContent = student.startYear;
         row.appendChild(startYearCell);
-
         const graduationYear = student.startYear + 4;
         const graduationYearCell = document.createElement('td');
         graduationYearCell.textContent = graduationYear;
         row.appendChild(graduationYearCell);
-
         const facultyCell = document.createElement('td');
         facultyCell.textContent = student.faculty;
         row.appendChild(facultyCell);
-
         studentsList.appendChild(row);
     });
 }
@@ -96,11 +80,9 @@ function calculateAge(dob) {
     const birthDate = new Date(dob);
     let age = currentDate.getFullYear() - birthDate.getFullYear();
     const monthDifference = currentDate.getMonth() - birthDate.getMonth();
-
     if (monthDifference < 0 || (monthDifference === 0 && currentDate.getDate() < birthDate.getDate())) {
         age--;
     }
-
     return age;
 }
 
@@ -111,20 +93,11 @@ function filterStudents() {
     const filterFaculty = document.getElementById('filter-faculty').value.toLowerCase();
     const filterStartYear = parseInt(document.getElementById('filter-start-year').value);
     const filterGraduationYear = parseInt(document.getElementById('filter-graduation-year').value);
-
     const filteredStudents = students.filter((student) => {
         const fullName = `${student.firstName} ${student.lastName} ${student.middleName}`.toLowerCase();
-
         return (
-            (!filterFirstName || fullName.startsWith(filterFirstName)) &&
-            (!filterLastName || fullName.includes(filterLastName)) &&
-            (!filterMiddleName || fullName.includes(filterMiddleName)) &&
-            (!filterFaculty || student.faculty.toLowerCase().includes(filterFaculty)) &&
-            (!filterStartYear || student.startYear === filterStartYear) &&
-            (!filterGraduationYear || student.startYear + 4 === filterGraduationYear)
-        );
+            (!filterFirstName || fullName.startsWith(filterFirstName)) && (!filterLastName || fullName.includes(filterLastName)) && (!filterMiddleName || fullName.includes(filterMiddleName)) && (!filterFaculty || student.faculty.toLowerCase().includes(filterFaculty)) && (!filterStartYear || student.startYear === filterStartYear) && (!filterGraduationYear || student.startYear + 4 === filterGraduationYear));
     });
-
     renderStudentsTable(filteredStudents);
 }
 
@@ -135,13 +108,9 @@ function resetFilters() {
     document.getElementById('filter-faculty').value = '';
     document.getElementById('filter-start-year').value = '';
     document.getElementById('filter-graduation-year').value = '';
-
     renderStudentsTable();
 }
-
 document.getElementById('add-student-form').addEventListener('submit', addStudent);
-
 document.getElementById('apply-filter-btn').addEventListener('click', filterStudents);
 document.getElementById('reset-filters-btn').addEventListener('click', resetFilters);
-
 renderStudentsTable();
